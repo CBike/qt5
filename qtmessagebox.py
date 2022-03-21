a4c15aecbf57c023cabac6dcd07c905af0bc560e
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-03-21 13:13:32 +0900

    qt4

--- qtmessagebox.py
+++ qtmessagebox.py
@@ -3,29 +3,30 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 
 
-class App(QWidget):
+class Exception_Notification(QWidget):
 
-    def show_QMessagebox(self,purpose,message):
+
+    def show_messagebox(self, purpose, message):
 
         if purpose == 'c':
-            QMessageBox.critical(self,'Critical', message, QMessageBox.Ok)
+            return QMessageBox.critical(self,'Critical', message, QMessageBox.Ok)
 
         elif purpose == 'q':
-            QMessageBox.question(self,'Question', message, QMessageBox.Ok | QMessageBox.No)
+            return QMessageBox.question(self,'Question', message, QMessageBox.Ok | QMessageBox.No)
 
         elif purpose == 'w':
-            QMessageBox.warning(self, 'Warning', message, QMessageBox.Ok)
+            return QMessageBox.warning(self, 'Warning', message, QMessageBox.Ok)
 
         elif purpose == 'i':
-            QMessageBox.information(self, 'Information', message, QMessageBox.Ok)
+            return QMessageBox.information(self, 'Information', message, QMessageBox.Ok)
+
+
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
 
-    ex.show_QMessagebox('c','critical')
-    ex.show_QMessagebox('i', 'information')
-    ex.show_QMessagebox('w', 'warning')
-    ex.show_QMessagebox('q', 'question')
+    noti = Exception_Notification()
+    result = noti.show_messagebox('q', 'question')
     sys.exit(app.exec_())
